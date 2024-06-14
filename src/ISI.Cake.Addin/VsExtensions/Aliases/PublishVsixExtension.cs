cb09d1d8f27e95954e9ef27ceee2d498e9d8b717
Ron Muth <ron.muth@isi-net.com>
2024-06-14 16:43:11 -0400

    Added PublishVsixExtension

---
+++ src/ISI.Cake.Addin/VsExtensions/Aliases/PublishVsixExtension.cs
@@ -0,0 +1,67 @@
+#region Copyright & License
+/*
+Copyright (c) 2024, Integrated Solutions, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+		* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+		* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+		* Neither the name of the Integrated Solutions, Inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#endregion
+ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISI.Extensions.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ISI.Cake.Addin.VsExtensions
+{
+	public static partial class Aliases
+	{
+		[global::Cake.Core.Annotations.CakeMethodAlias]
+		public static PublishVsixExtensionResponse PublishVsixExtension(this global::Cake.Core.ICakeContext cakeContext, IPublishVsixExtensionRequest request)
+		{
+			ServiceProvider.Initialize();
+
+			var response = new PublishVsixExtensionResponse();
+
+			var logger = new CakeContextLogger(cakeContext);
+
+			var dateTimeStamper = ISI.Extensions.ServiceLocator.Current.GetService<ISI.Extensions.DateTimeStamper.IDateTimeStamper>();
+			var jsonSerializer = ISI.Extensions.ServiceLocator.Current.GetService<ISI.Extensions.JsonSerialization.IJsonSerializer>();
+			var configuration = ISI.Extensions.ServiceLocator.Current.GetService<ISI.Extensions.VisualStudio.Configuration>();
+			var nugetApi = new ISI.Extensions.Nuget.NugetApi(ISI.Extensions.ServiceLocator.Current.GetService<ISI.Extensions.Nuget.Configuration>(), logger, jsonSerializer);
+
+			var vsixPublisherApi = new ISI.Extensions.VisualStudio.VsixPublisherApi(logger, jsonSerializer, new ISI.Extensions.VisualStudio.VsWhereApi(configuration, logger, dateTimeStamper, nugetApi));
+
+			vsixPublisherApi.PublishVsix(new()
+			{
+				PublisherKey = (request as PublishVsixExtensionRequest)?.PublisherKey ?? (request as PublishVsixExtensionUsingSettingsRequest)?.Settings?.VsExtensions?.PublisherKey ?? string.Empty,
+				PersonalAccessToken = (request as PublishVsixExtensionRequest)?.PersonalAccessToken ?? (request as PublishVsixExtensionUsingSettingsRequest)?.Settings?.VsExtensions?.PublisherPersonalAccessToken ?? string.Empty,
+
+				InternalName = request.InternalName,
+				Version = request.Version,
+
+				VsixFullName = request.VsixFullName,
+				VsixManifestFullName = request.VsixManifestFullName,
+
+				ReadMeFullName = request.ReadMeFullName,
+				PriceCategory = ISI.Extensions.VisualStudio.DataTransferObjects.VsixPublisherApi.GenerateVsixPublishManifestRequestPricingCategory.Free,
+				Categories = request.Categories,
+				Tags = request.Tags,
+
+				AddToLog = (logEntryLevel, description) => logger.LogInformation(description),
+			});
+
+			return response;
+		}
+	}
+}
